37a8d49dd6aa1e2cf8d2242b76e82e014481b9b9
Nguyen Duc Anh <sin2k.dev@gmail.com>
2022-03-29 18:04:04 +0700

    Login Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,39 @@
-import './App.css';
+import { useRef, useEffect } from "react";
+import Login from "./pages/login/Login";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+
+  const cursorRef = useRef();
+  const cursorRef2 = useRef();
+
+  useEffect(() => {
+      const handleUserMouseMove = (e) => {
+          cursorRef.current.style.cssText = cursorRef2.current.style.cssText =
+              "left: " + e.clientX + "px; top: " + e.clientY + "px;";
+      };
+
+      window.addEventListener("mousemove", handleUserMouseMove);
+
+      return () => {
+          window.removeEventListener("mousemove", handleUserMouseMove);
+      };
+  }, []);
+
   return (
-    <div className="App">
-        <h1>Welcome to Netflix app</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
+
+      <div className="cursor" ref={cursorRef}></div>
+      <div className="cursor2" ref={cursorRef2}></div>
+    </Router>
   );
 }
 
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,36 @@
+import "./login.scss";
+
+export default function Login() {
+
+    const handleContextMenu = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <div className="login" onContextMenu={handleContextMenu}>
+            <div className="login__logo">
+                <img
+                    className="login__logo-img"
+                    src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
+                    alt=""
+                />
+            </div>
+
+            <div className="login__wrap">
+                <form className="login__form">
+                    <h1>Sign In</h1>
+                    <input type="email" placeholder="Email or phone number" className="login__input"/>
+                    <input type="password" placeholder="Password" className="login__input"/>
+                    <button className="login__btn">Sign In</button>
+                    <span className="login__span">
+                        New to Netflix? <b>Sign up now.</b>
+                    </span>
+                    <small>
+                        This page is protected by Google reCAPTCHA to ensure
+                        you're not a bot. <b>Learn more</b>.
+                    </small>
+                </form>
+            </div>
+        </div>
+    );
+}
